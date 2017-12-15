225be114bfcff3dd7e1116772e6af01547b457ef
Abanum Chuks <oilmonegov@gmail.com>
2017-12-15 22:25:51 +0100

    Added Admin Model and Controller. Added Routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Users Auth Route
+Auth::routes();
+
+// Post-Auth Route
+Route::get('/home', 'HomeController@index')->name('home');
+
+//admin route for our multi-auth system
+Route::get('/admin','AdminController@index')->name('admin');
